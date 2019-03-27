426d1a738a64e75b4753b2b6aef6b43bb2a32f32
wlandau-lilly <will.landau@lilly.com>
2019-03-27 10:49:56 -0400

    Tune on batch size

--- R/plan2.R
+++ R/plan2.R
@@ -1,16 +1,16 @@
-dropout_rate <- seq(0.1, 0.4, by = 0.1)
+batch_sizes <- c(16, 32, 256)
 
 plan <- drake_plan(
   data = read_csv(file_in("data/customer_churn.csv"), col_types = cols()) %>%
     initial_split(prop = 0.3),
   rec = prepare_recipe(data),
   model = target(
-    train_model(data, rec, dropout),
-    transform = map(dropout = !!dropout_rate)
+    train_model(data, rec, batch_size),
+    transform = map(batch_size = !!batch_sizes)
   ),
   conf = target(
     confusion_matrix(data, rec, model),
-    transform = map(model)
+    transform = map(model, .id = batch_size)
   ),
   comparison = target(
     compare_models(conf),
